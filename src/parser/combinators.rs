0528887da1ddf1720187408a0c6c44cc38f072ab
Jacob Reckhard <jacobreckhard@gmail.com>
2019-07-15 22:37:22 +0000

    added or combinator

--- src/parser/combinators.rs
+++ src/parser/combinators.rs
@@ -23,7 +23,24 @@ where
             Err(input)
         }
     }
+}
 
+fn or<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (Option<R1>, Option<R2>)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| {
+        if let Ok(data) = parser1(input) {
+            Ok((data.0, (Some(data.1), None)))
+        } else {
+            if let Ok(data) = parser2(input) {
+                Ok((data.0, (None, Some(data.1))))
+            } else {
+                Err(input)
+            }
+        }
+    }
 }
 
 
@@ -54,7 +71,7 @@ mod test {
     }
 
     #[test]
-    fn test_pair_a() {
+    fn test_and_a() {
         let ba = and(lit("b"), lit("a"));
 
         let res = ba("banana");
@@ -64,6 +81,24 @@ mod test {
 
         let res = ba("apple");
         assert_eq!(res.expect_err("This test should fail"), "apple");
+    }
+
+    #[test]
+    fn test_or_a() {
+        let ba = or(lit("b"), lit("a"));
+
+        let res = ba("banana");
+        let data = res.expect("This test should pass");
+        assert_eq!(data.0, "anana");
+        assert_eq!(data.1, (Some("b".to_string()), None));
+
+        let res = ba("apple");
+        let data = res.expect("This test should pass");
+        assert_eq!(data.0, "pple");
+        assert_eq!(data.1, (None, Some("a".to_string())));
+
+        let res = ba("orange");
+        assert_eq!(res.expect_err("This test should fail"), "orange");
     }
 
 }
