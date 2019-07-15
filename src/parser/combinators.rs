7bffd19541033e7f3074e6f86282b4af86bd3382
Jacob Reckhard <jacobreckhard@gmail.com>
2019-07-15 22:27:40 +0000

    started adding combinators

---
+++ src/parser/combinators.rs
@@ -0,0 +1,69 @@
+pub fn lit(expected: &'static str) -> impl Fn(&str) -> Result<(&str, String), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => {
+            Ok((&input[expected.len()..], (String::from(expected))))
+        }
+        _ => Err(input),
+    }
+}
+
+fn and<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| {
+        if let Ok(data) = parser1(input) {
+            if let Ok(data2) = parser2(data.0) {
+                Ok((data2.0, (data.1, data2.1)))
+            } else {
+                Err(input)
+            }
+        } else {
+            Err(input)
+        }
+    }
+
+}
+
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_char_a() {
+        let a = lit("a");
+        let res = a("apple");
+        assert_eq!(res.expect("This test should pass").0, "pple");
+
+        let res = a("banana");
+        assert_eq!(res.expect_err("This test should fail"), "banana");
+    }
+
+    #[test]
+    fn test_char_b() {
+        let b = lit("b");
+        let res = b("banana");
+        assert_eq!(res.expect("This test should pass").0, "anana");
+
+        let res = b("apple");
+        assert_eq!(res.expect_err("This test should fail"), "apple");
+    }
+
+    #[test]
+    fn test_pair_a() {
+        let ba = and(lit("b"), lit("a"));
+
+        let res = ba("banana");
+        let data = res.expect("This test should pass");
+        assert_eq!(data.0, "nana");
+        assert_eq!(data.1, ("b".to_string(), "a".to_string()));
+
+        let res = ba("apple");
+        assert_eq!(res.expect_err("This test should fail"), "apple");
+    }
+
+}
